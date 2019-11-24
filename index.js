93f084f4da6dcf3f021e850406e2fbde1ee0c08b
Visnu Pitiyanuvath <visnup@gmail.com>
2019-11-24 11:43:17 -0800

    simplify

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
-'use strict';
 const options = {
-  originWhitelist: ['https://visnup.static.observableusercontent.com'],
-  requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
+  originWhitelist: ["https://visnup.static.observableusercontent.com"],
+  requireHeader: ["origin", "x-requested-with"],
+  removeHeaders: ["cookie"]
 };
-const cors_proxy = require('cors-anywhere').createServer(options);
+const cors_proxy = require("cors-anywhere").createServer(options);
 module.exports = (req, res) => {
-  console.log('headers', req.headers);
-  console.log('url', req.url);
-  cors_proxy.emit('request', req, res);
+  console.log(req.url);
+  cors_proxy.emit("request", req, res);
 };
