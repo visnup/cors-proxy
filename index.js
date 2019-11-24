6bc557f9dbb65a91df442643fe51c60f86e9b475
Visnu Pitiyanuvath <visnup@gmail.com>
2019-11-24 11:47:45 -0800

    get origins from environment

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
-const options = {
-  originWhitelist: ["https://visnup.static.observableusercontent.com"],
+const cors_proxy = require("cors-anywhere").createServer({
+  originWhitelist: JSON.parse(process.env["cors-origins"]),
   requireHeader: ["origin", "x-requested-with"],
   removeHeaders: ["cookie"]
-};
-const cors_proxy = require("cors-anywhere").createServer(options);
+});
 module.exports = (req, res) => {
   console.log(req.url);
   cors_proxy.emit("request", req, res);
